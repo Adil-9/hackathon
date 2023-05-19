25508ec9ae5b9975c2d5318472ea85ef645bd209
Adil-9 <96561291+Adil-9@users.noreply.github.com>
2023-05-19 13:55:13 +0600

    Add files via upload

--- test/trash/routes.go
+++ test/trash/routes.go
@@ -2,7 +2,9 @@ package trash
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 var (
@@ -18,7 +20,13 @@ func Home(w http.ResponseWriter, r *http.Request) {
 func Person(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("templates/person.html")
 	Posts, _ := repoo.FindAll()
-	post := Posts[0]
+	lent := len(Posts)
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate a random number between 1 and x
+	randomNumber := rand.Intn(lent)
+	post := Posts[randomNumber]
 
 	tmpl.Execute(w, post)
 }
